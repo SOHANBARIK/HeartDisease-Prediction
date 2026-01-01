47b54cd1e37940b8e11514070be89bcd0ee5f6db
Sohan Barik <sohanbarik227@gmail.com>
2026-01-01 08:47:18 +0530

    did some changes in message

--- frontend/src/components/HeartFormModal.jsx
+++ frontend/src/components/HeartFormModal.jsx
@@ -135,7 +135,7 @@ const HeartFormModal = ({ close }) => {
       console.error("Prediction Failed", err);
       
       // ⚠️ ALERT THE USER (Added Logic)
-      alert("Connection to the AI server was lost. Please refresh the page or try again in a moment.");
+      alert("Connection to the AI server was lost. Please refresh the page or login again.");
       
       setError(err.message || "Prediction failed");
     } finally {
